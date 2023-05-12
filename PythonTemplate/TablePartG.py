f00b8e60e6546c7e2041e207499c10e710b05055
Sotheara Chea (Theara) <59755034+ctheara@users.noreply.github.com>
2023-05-12 02:12:19 -0400

    Update TablePartG.py

--- PythonTemplate/TablePartG.py
+++ PythonTemplate/TablePartG.py
@@ -3,14 +3,22 @@ import happybase as hb
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 
-color = ???
-name = ???
-power = ???
+connection = hb.Connection()
+table = connection.table('powers')
 
-color1 = ???
-name1 = ???
-power1 = ???
+# Scan the 'powers' table
+data = list(table.scan())
 
-print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
-
-
+# Iterate over each row
+for key, value in data:
+    color = value[b'custom:color']
+    name = value[b'professional:name']
+    power = value[b'personal:power']
+    # Iterate over each row again to find matching color
+    for key1, value1 in data:
+        color1 = value1[b'custom:color']
+        name1 = value1[b'professional:name']
+        power1 = value1[b'personal:power']
+        # If color matches and names are different, print them
+        if color == color1 and name != name1:
+            print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
